6af1ff26435faa58a427ce595be6bccddf226f95
Saurabh S <saurabh.s@thinkbridge.in>
2024-07-20 16:27:51 +0530

    Created login, landing-page, signup,user-home components and installed bootstrap

--- PassPortal-Alpha/src/app/app.routes.ts
+++ PassPortal-Alpha/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserHomeComponent } from './components/user-home/user-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'user-home', component: UserHomeComponent },
+];
