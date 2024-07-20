7b3d49f689d36b77a06d16e773028913df42b93a
Saurabh S <saurabh.s@thinkbridge.in>
2024-07-20 19:50:11 +0530

    Page not found 404 page

--- PassPortal-Alpha/src/app/app.routes.ts
+++ PassPortal-Alpha/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LandingPageComponent } from './components/landing-page/landing-page.com
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { UserHomeComponent } from './components/user-home/user-home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'user-home', component: UserHomeComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
--- PassPortal-Alpha/src/app/components/user-home/user-home.component.ts
+++ PassPortal-Alpha/src/app/components/user-home/user-home.component.ts
@@ -1,9 +1,10 @@
+import { UserNavbarComponent } from './../user-navbar/user-navbar.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-home',
   standalone: true,
-  imports: [],
+  imports: [UserNavbarComponent],
   templateUrl: './user-home.component.html',
   styleUrl: './user-home.component.css'
 })
