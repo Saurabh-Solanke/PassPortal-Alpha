020120b62df4b7c9b9d3e4a5f9e40ed17caec409
Saurabh S <saurabh.s@thinkbridge.in>
2024-07-23 14:13:55 +0530

    Landing page

--- PassPortal-Alpha/src/app/components/landing-page/landing-page.component.ts
+++ PassPortal-Alpha/src/app/components/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent,RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
